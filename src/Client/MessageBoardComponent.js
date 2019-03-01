44e85b2e5d7f3f45ed52e8473d78b033f146dc7a
Ajinkya Kakade <AjinkyaK@talentica-all.com>
2019-03-01 11:51:57 +0530

    visual enhancement

--- src/Client/MessageBoardComponent.js
+++ src/Client/MessageBoardComponent.js
@@ -5,7 +5,7 @@ function MessageBoardComponent(props) {
   return (  
     <ul className="list-group">
       {props.messages.map(item => (
-        <li className="list-group-item" key={item}>{item}</li>
+        <li className="list-group-item list-group-item-success" key={item}>{item}</li>
       ))}
     </ul>
   );
